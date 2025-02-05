c8045d76a44b6671b1cb89c8ae37d1476ea5ba33
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-02-05 20:00:51 +0000

    write a failing test

--- appointments/test/Appointment.test.ts
+++ appointments/test/Appointment.test.ts
@@ -1,7 +1,12 @@
+import { render } from "@testing-library/react";
+import { Appointment } from "../src/Appointment";
 describe("Appointment", () => {
   it("renders the customer first name", () => {
-    const component = "??????";
-    const container = "??????";
+    const customer = { firstName: "Ashley" };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+    render(component);
     expect(document.body.textContent).toMatch("Ashley");
   });
 });
