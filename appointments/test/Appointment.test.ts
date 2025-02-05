e549c704f4a1c908e95669112faf21d067e239a3
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-02-05 19:55:44 +0000

    create varianle holders

--- appointments/test/Appointment.test.ts
+++ appointments/test/Appointment.test.ts
@@ -1,5 +1,7 @@
 describe("Appointment", () => {
   it("renders the customer first name", () => {
+    const component = "??????";
+    const container = "??????";
     expect(document.body.textContent).toMatch("Ashley");
   });
 });
