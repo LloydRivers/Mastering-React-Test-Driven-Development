48d287b8f460e223db9b4e78b2db48b90b68c362
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-02-06 08:13:53 +0000

    remove .contaner syntax

--- appointments/test/Appointment.test.tsx
+++ appointments/test/Appointment.test.tsx
@@ -1,21 +1,25 @@
 import { render } from "@testing-library/react";
 import { Appointment } from "../src/components/Appointment";
+
 type CustomerType = {
   firstName: string;
 };
+
 describe("Appointment", () => {
-  let container: HTMLElement;
   let customer: CustomerType;
+  beforeEach(() => {
+    customer = { firstName: "" };
+  });
 
   it("renders the customer first name (Ashley)", () => {
-    customer = { firstName: "Ashley" };
-    container = render(<Appointment customer={customer} />).container;
+    customer.firstName = "Ashley";
+    const { container } = render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch("Ashley");
   });
 
   it("renders the customer first name (Jordan)", () => {
-    customer = { firstName: "Jordan" };
-    container = render(<Appointment customer={customer} />).container;
+    customer.firstName = "Jordan";
+    const { container } = render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch("Jordan");
   });
 });
